b945f3844d638552c8d854afa7230d46c9f2b75d
JapaProgramadora <76882368+JapaProgramadora@users.noreply.github.com>
2023-08-17 10:46:35 -0300

    updates on templates

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props => {
+    <Main icon="home" title="Início" 
+        subtitle="Segundo Projeto do Capítulo de React">
+            <div className="display-4">Bem Vindo!</div>
+            <hr/>
+            <p className="mb-0">Sistema para exemplificar a
+            construção de um cadadstro de desenvolvimento em React!</p>
+    </Main>
+}
